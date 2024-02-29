6982b7d6d115166a34318e7c634e0d372bd126c8
olegMoranovich <126958363+OlenaKliuchka@users.noreply.github.com>
2024-02-29 18:57:40 +0200

    finish-hw-02

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,10 +1,21 @@
-function getElementWidth(content, padding, border) {
-  const numberContent = Number.parseFloat(content);
-  const numberPadding = Number.parseFloat(padding);
-  const numberBorder = Number.parseFloat(border);
-  const totalElements = numberContent + 2 * (numberPadding + numberBorder);
-  return totalElements;
+function checkForSpam(message) {
+let result;
+
+ const lowerCaseMessage = message.toLowerCase();
+
+ result = lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")
+ 
+ return result;
 }
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+
+
+
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
